f16cf6a03da8e40e309012878ed7dc2549e9168b
Andy LERANDY <alerandy@student.42.fr>
2018-06-13 17:15:54 +0200

    Nouveau plan de travail.

---
+++ htdocs/index.php
@@ -0,0 +1,25 @@
+<doctype/>
+<html>
+<head>
+	<title>Camagru</title>
+	<link rel="stylesheet" type="text/css" href="css/default.css" />
+	<meta charset="utf-8" />
+</head>
+<body>
+	<header>
+		<nav>
+		</nav>
+	</header>
+	<div id="templateContainer">
+		<div id="img_container">
+		</div>
+		<div id="sidebar">
+		</div>
+	</div>
+	<footer>
+		<div id="fnav">
+
+		</div>
+	</footer>
+</body>
+</html>
