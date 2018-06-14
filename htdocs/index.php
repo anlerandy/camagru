eff293a7eb97e3460f0d8a8698073a2ce1046225
Andy LERANDY <alerandy@student.42.fr>
2018-06-14 20:10:47 +0200

    Sauvegarde simple : Début de CSS Ajout des premiers assets

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,13 +1,26 @@
-<doctype/>
 <html>
 <head>
 	<title>Camagru</title>
 	<link rel="stylesheet" type="text/css" href="css/default.css" />
+	<link rel="icon" type="image/ico" href="img/favicon.ico" />
 	<meta charset="utf-8" />
 </head>
 <body>
 	<header>
+		<div id="title">
+				<h3><span style="font-size:40px;">PhotoBOMB!</span><br/>The website</h3>
+				<h1>Camagru</h1>
+		</div>
 		<nav>
+			<div id="menu">
+			<a href="#">Home</a>
+			<a href="#">Galery</a>
+			<a href="#">Post</a>
+			</div>
+			<div id="log">
+			<a href="#">LogIn</a>
+			<a href="#">SignUp</a>
+			</div>
 		</nav>
 	</header>
 	<div id="templateContainer">
