48b460d9d701d0c99147516af79565a465d673f0
anlerandy <alerandy@student.42.fr>
2018-06-19 13:27:08 +0200

    Sauvegarde simple : Réparation de db_conn()

--- htdocs/database/libdb.php
+++ htdocs/database/libdb.php
@@ -38,7 +38,7 @@ function db_conn()
 					$db->exec("CREATE TABLE `$r_db`.`users` ( `id` INT NOT NULL AUTO_INCREMENT , `login` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , `pass` VARCHAR(255) NOT NULL , `level` INT NULL DEFAULT NULL , `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, `mail` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , UNIQUE (`id`), UNIQUE (`login`), UNIQUE (`mail`)) ENGINE = InnoDB;");
 					$db->exec("CREATE TABLE `$r_db`.`images` ( `id` INT NOT NULL AUTO_INCREMENT , `user_id` INT NOT NULL , `likes` INT NULL DEFAULT NULL , `date` DATE NOT NULL, `state` INT NULL DEFAULT NULL , PRIMARY KEY (`user_id`, `date`), UNIQUE (`id`)) ENGINE = InnoDB;");
 					$u_passwd = hash('whirlpool', $r_passwd);
-					$db->exec("INSERT INTO `camagru`.`users`(`login`, `pass`, `level`, `mail`) VALUES ('admin','$u_passwd',3,'admin@admin.fr')");
+					$db->exec("INSERT INTO `$r_db`.`users`(`login`, `pass`, `level`, `mail`) VALUES ('admin','$u_passwd',3,'admin@admin.fr')");
 				}
 		}
 		return ($db);
